49bb84ad6e8e04d34044910a486e89ed93b9e10e
abbas <86982819+abbassiriyus@users.noreply.github.com>
2024-05-08 12:17:17 +0500

    2
    2

--- index.js
+++ index.js
@@ -5,24 +5,38 @@ const TelegramBot = require('node-telegram-bot-api');
 // Telegram bot API kaliti
 const token = '6492572397:AAGmkt8yP7NrFhGwnUUJyxFwhlCcF1878Rs';
 
-// Botni yaratish
-const bot = new TelegramBot(token, { polling: true });
-
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  console.log(msg);
-  // Guruhdagi foydalanuvchilarni aniqlash
-  if (msg.chat.type === 'supergroup' || msg.chat.type === 'group') {
-    bot.getChatMembersCount(chatId).then((count) => {
-      // Guruhdagi foydalanuvchilar sonini olish
-      console.log(`Guruhdagi foydalanuvchilar soni: ${count}`);
+// const messageData = {
+//     message_id: 2,
+//     from: {
+//       id: 1520593027,
+//       is_bot: false,
+//       first_name: '𝖆𝖇𝖇𝖆𝖘',
+//       username: 'dsadsa111',
+//       language_code: 'en'
+//     },
+//     chat: { id: , title: 'asda', type: 'supergroup' },
+//     date: 1715151938,
+//     text: 'salom'
+//   };
+  
+  const chatId = -1002055646694;
+  
+ 
+  // Botni yaratish
+  const bot = new TelegramBot(token, { polling: true });
+  
+  bot.getChatMembersCount(chatId).then((count) => {
+    console.log(`Guruhdagi foydalanuvchilar soni: ${count}`);
+  });
+  
+  bot.getChatMembers(chatId).then((members) => {
+    let userBotCount = 0;
+  
+    members.forEach((member) => {
+      if (member.username && member.username.toLowerCase().includes('_bot')) {
+        userBotCount++;
+      }
     });
-
-    bot.getChatMembers(chatId).then((members) => {
-      // Guruhdagi foydalanuvchilarni olish
-      members.forEach((member) => {
-        console.log(`Foydalanuvchi ID: ${member.user.id}, Ismi: ${member.user.first_name}`);
-      });
-    });
-  }
-});
+  
+    console.log(`"${messageData.chat.title}" guruhida ${userBotCount} ta "user_bot" mavjud.`);
+  });
